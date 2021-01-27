7e7184e24794d4a70000da3fa4ac85bfb4c6749b
Davide Heller <davide.heller@srf.ch>
2021-01-27 18:02:53 +0100

    add simple client call with requests and access credentials in .env

--- pdp_graphql_client_python/cli.py
+++ pdp_graphql_client_python/cli.py
@@ -2,15 +2,35 @@
 
 """Console script for pdp_graphql_client_python."""
 import sys
+import json
 import click
+from dotenv import load_dotenv
+
+from pdp_graphql_client_python.client import run_query
+
+load_dotenv()  # take environment variables from .env
 
 
 @click.command()
-def main(args=None):
+@click.option('--query', help='GraphQL query', default='''
+{
+    faroItemsByPlayUrn(
+        urns: [
+            "urn:srf:video:00025f95-2437-4dc3-a15a-44e5d2fa1d37",
+            "urn:srf:video:f0076ff4-6f9a-48d8-a61c-83ad203b9f62"
+        ]
+    ) {
+        producer
+        program {
+            department
+        }
+    }
+}
+''')
+def main(query):
     """Console script for pdp_graphql_client_python."""
-    click.echo("Replace this message by putting your code into "
-               "pdp_graphql_client_python.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    result = run_query(query)
+    print(json.dumps(result, indent=1))
     return 0
 
 
--- pdp_graphql_client_python/client.py
+++ pdp_graphql_client_python/client.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import os
+import requests
+
+
+def run_query(query):
+    """Queries GraphQL API with input query and returns json request"""
+    request = requests.post(
+            os.getenv('PDP_API'),
+            json={'query': query},
+            auth=(os.getenv('USER_NAME'), os.getenv('USER_PASSWORD')),
+            )
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed with code {}. {}".format(request.status_code, query))
