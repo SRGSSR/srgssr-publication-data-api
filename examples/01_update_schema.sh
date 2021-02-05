ae2ed9831b12d134489af5d9dd13ee6b7938bd2b
Davide Heller <davide.heller@srf.ch>
2021-02-05 13:53:42 +0100

    SRGPDP-800: Combined step 02 into updated script and added safety checks

--- examples/01_update_schema.sh
+++ examples/01_update_schema.sh
@@ -1,7 +1,27 @@
+#!/bin/bash
+
+# check if environment variables are defined
+if [ -z "$USER_NAME" ] || [ -z "$USER_PASSWORD" ] || [ -z "$PDP_API" ]; then
+    echo "ERROR - One or more variables of [ USER_NAME, USER_PASSWORD, PDP_API ] were not found!"
+    echo "Please export your .env credentials as outlined in README before running the script"
+    exit 1
+fi
+
+echo "INFO - Starting schema update from ${PDP_API}"
+
+# compute the basic authentication token
+BASE64_AUTH_TOKEN=$(echo -n "${USER_NAME}:${USER_PASSWORD}" |  base64)
+
+# Introspection call to retrieve JSON schema from GraphQL Endpoint
 # source: https://github.com/profusion/sgqlc#code-generator
 python3 -m sgqlc.introspection \
      --exclude-deprecated \
      --exclude-description \
      -H "Authorization: Basic ${BASE64_AUTH_TOKEN}" \
      https://graphql-api.pdp.dev.srgssr.ch/graphql \
-     pdp_schema.json
+     pdp_schema.json || exit 1
+
+echo "INFO - Successfully downloaded pdp_schema.json"
+
+# source: https://github.com/profusion/sgqlc#code-generator
+sgqlc-codegen schema pdp_schema.json pdp_schema.py && echo "INFO - Successfully updated pdp_schema.py"
--- examples/02_generate_pyclass.sh
+++
@@ -1,2 +0,0 @@
-# source: https://github.com/profusion/sgqlc#code-generator
-sgqlc-codegen schema pdp_schema.json pdp_schema.py
