66827e6cd9e54873f4dddd9ed6bf95b3ad04f697
Rico Metzger <rico.metzger@gmail.com>
2021-03-24 14:35:31 +0100

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SRGSSR/pdp_graphql_client_python',
-    version='0.4.0',
+    version='0.4.1',
     zip_safe=False,
 )
 
